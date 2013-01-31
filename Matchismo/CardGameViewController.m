dc716ac4d8f80158f15660f83af667873831110a
Joey Smith <joeysmith@Collings.local>
2013-01-30 20:12:41 -0800

    Final version for HW0

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,23 +7,46 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
+/// Auto generated space for private properties
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+@property (strong, nonatomic) PlayingCardDeck *theDeck;
+@property (strong, nonatomic) PlayingCard *aCard;
 @end
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+/// Perform lazy instantiation by creating theDeck when the getter
+/// for theDeck is called the first time.
+- (PlayingCardDeck *) theDeck {
+    if (!_theDeck) _theDeck = [[PlayingCardDeck alloc] init];
+    return _theDeck;
 }
 
-- (void)didReceiveMemoryWarning
+/// Define setter for flipCount
+- (void) setFlipCount:(int)flipCount
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+}
+
+/// Action defined when a button is pressed and released.
+- (IBAction)flipCard:(UIButton *)sender {
+    sender.selected = !sender.isSelected;
+    if (sender.isSelected) {
+        self.aCard = (PlayingCard*)self.theDeck.drawRandomCard;
+        if (self.aCard != nil) {
+            // Use the PlayingCard description
+            [sender setTitle:[NSString stringWithFormat:@"%@", self.aCard] forState:UIControlStateSelected];
+        } else {
+            [sender setTitle:[NSString stringWithFormat:@"MT"] forState:UIControlStateSelected];
+        }
+        self.flipCount++;
+    }
 }
 
 @end
