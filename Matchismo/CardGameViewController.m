38f352bbec4f74acdab006d3b3d212ff39595785
Joey Smith <joeysmith@Collings.local>
2013-02-20 19:11:40 -0800

    remove commented out code.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -15,7 +15,6 @@
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-//@property (strong, nonatomic) Deck *theDeck;
 @property (strong, nonatomic) IBOutletCollection(UIView) NSArray *cardButtons;
 @property (strong, nonatomic) CardMatchingGame *game;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
@@ -23,31 +22,18 @@
 
 @implementation CardGameViewController
 
-// Perform lazy instantiation by creating theDeck when the getter
-// for theDeck is called the first time.
-//- (Deck *) theDeck {
-//    if (!_theDeck) _theDeck = [[PlayingCardDeck alloc] init];
-//    return _theDeck;
-//}
 
-// LI for the game.
+// Lazy instantiation for the game.
 - (CardMatchingGame *) game {
-    /// Allocate the deck wof cards on the fly, we no longer need theDeck object.
+    // Allocate the deck wof cards on the fly, we no longer need theDeck object.
     if(!_game) _game = [[CardMatchingGame alloc] initWithCardCount:self.cardButtons.count
                                                          usingDeck:[[PlayingCardDeck alloc] init]];
-;
     return _game;
 }
 
 - (void)setCardButtons:(NSArray *)cardButtons {
     _cardButtons = cardButtons;
     [self updateUI];
-    
-    /*
-     for (UIButton *cardButton in cardButtons) {
-     Card *card = [self.theDeck drawRandomCard];
-     [cardButton setTitle:card.contents forState:UIControlStateSelected];
-     */
 }
 
 - (void) updateUI{
@@ -67,8 +53,7 @@
 }
 
 // Define setter for flipCount
-- (void) setFlipCount:(int)flipCount
-{
+- (void) setFlipCount:(int)flipCount {
     _flipCount = flipCount;
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
 }
